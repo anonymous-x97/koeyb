a3c1d63334a470ca9735b269e9d8317861978bad
Ryuk <90273333+anonymous-x97@users.noreply.github.com>
2022-04-26 20:29:19 +0530

    Update checks.sh

--- init/checks.sh
+++ init/checks.sh
@@ -9,27 +9,9 @@
 # All rights reserved.
 
 
-_installbuildessentials() {
+_b() {
     apt update && apt upgrade -y && apt install -y --no-install-recommends apt-utils curl git gnupg2 wget unzip tree gcc python3-dev zlib1g-dev apt-transport-https build-essential coreutils jq pv ffmpeg mediainfo neofetch p7zip-full libfreetype6-dev libjpeg-dev libpng-dev libgif-dev libwebp-dev
     log " installling essentials"
-}
-
-_installchromes() {
-    log " installing chrome"
-    mkdir -p /tmp/ && \
-    cd /tmp/ && \
-    wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb && \
-    dpkg -i ./google-chrome-stable_current_amd64.deb; apt -fqqy install && \
-    rm ./google-chrome-stable_current_amd64.deb
-}
-
-_installchromed() {
-    log " installling chrome"
-    mkdir -p /tmp/ && \
-    cd /tmp/ && \
-    wget -O /tmp/chromedriver.zip http://chromedriver.storage.googleapis.com/$(curl -sS chromedriver.storage.googleapis.com/LATEST_RELEASE)/chromedriver_linux64.zip && \
-    unzip /tmp/chromedriver.zip chromedriver -d /usr/bin/ && \
-    rm /tmp/chromedriver.zip
 }
 
 _checkConfigFile() {
@@ -145,9 +127,7 @@ _flushMessages() {
 }
 
 assertPrerequisites() {
-    _installbuildessentials
-    _installchromes
-    _installchromed
+    _b
     _checkConfigFile
     _checkRequiredVars
 }
