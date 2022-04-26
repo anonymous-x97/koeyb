95616a5b753fb964aa0442633632dbc011d66a2e
Ryuk <90273333+anonymous-x97@users.noreply.github.com>
2022-04-26 19:52:08 +0530

    Update checks.sh

--- init/checks.sh
+++ init/checks.sh
@@ -12,6 +12,42 @@
 #    log "Checking Bash Commands ..."
 #    command -v jq &> /dev/null || quit "Required command : jq : could not be found !"
 #}
+
+_installbuildessentials() {
+    apt -qq update && apt -qq upgrade -y && \
+    apt -qq install -y --no-install-recommends \
+    apt-utils \
+    curl \
+    git \
+    gnupg2 \
+    wget \
+    unzip \
+    tree \
+    gcc zlib1g-dev \
+    apt-transport-https \
+    build-essential coreutils jq pv \
+    ffmpeg mediainfo \
+    neofetch \
+    p7zip-full \
+    libfreetype6-dev libjpeg-dev libpng-dev libgif-dev libwebp-dev\
+}
+
+_installchromes() {
+    mkdir -p /tmp/ && \
+    cd /tmp/ && \
+    wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb && \
+    dpkg -i ./google-chrome-stable_current_amd64.deb; apt -fqqy install && \
+    rm ./google-chrome-stable_current_amd64.deb
+}
+
+_installchromed() {
+    mkdir -p /tmp/ && \
+    cd /tmp/ && \
+    wget -O /tmp/chromedriver.zip http://chromedriver.storage.googleapis.com/$(curl -sS chromedriver.storage.googleapis.com/LATEST_RELEASE)/chromedriver_linux64.zip && \
+    unzip /tmp/chromedriver.zip chromedriver -d /usr/bin/ && \
+    rm /tmp/chromedriver.zip
+}
+
 _checkConfigFile() {
     log "Checking Config File ..."
     configPath="config.env"
@@ -114,7 +150,7 @@ _checkPaths() {
 _checkUpstreamRepo() {
     remoteIsExist $UPSTREAM_REMOTE || addUpstream
     editLastMessage "Fetching Data From UPSTREAM_REPO..."
-    fetchUpstream || updateUpstream && fetchUpstream || quit "Invalid UPSTREAM_REPO var !"
+    fetchUpstream || updateUpstream && fetchUpstream || "Invalid UPSTREAM_REPO var !"
     fetchBranches
     updateBuffer
 }
@@ -154,6 +190,9 @@ _flushMessages() {
 }
 
 assertPrerequisites() {
+    _installbuildessentials
+    _installchromes
+    _installchromed
 #    _checkBashReq
     _checkConfigFile
     _checkRequiredVars
